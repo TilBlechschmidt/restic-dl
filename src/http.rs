60ae4aba2fc217ab152653df37bb25824281aeaa
Til Blechschmidt <me@tibl.dev>
2024-06-09 00:07:16 +0200

    Add support for systemd sockets

--- src/http.rs
+++ src/http.rs
@@ -1,4 +1,5 @@
 use crate::args::ServerArgs;
+use listenfd::ListenFd;
 
 mod extract;
 mod middleware;
@@ -6,12 +7,21 @@ mod navigation;
 mod route;
 
 pub use middleware::{repository::CookieParameters, session::SessionCache};
+use tokio::net::TcpListener;
 
 pub async fn serve(args: ServerArgs) -> std::result::Result<(), Box<dyn std::error::Error>> {
-    let listener = tokio::net::TcpListener::bind(args.address).await?;
-    let app = args.into_layers(route::router());
+    let mut listenfd = ListenFd::from_env();
+
+    let listener = if let Some(listener) = listenfd.take_tcp_listener(0)? {
+        println!("Using provided socket, address and port from args are ignored");
+        TcpListener::from_std(listener)?
+    } else {
+        TcpListener::bind(args.address).await?
+    };
 
     println!("Listening on {}", listener.local_addr()?);
+
+    let app = args.into_layers(route::router());
     axum::serve(listener, app).await?;
 
     Ok(())
